279412bc5a7247ab07e6b32226f834b853cb1a50
Gustavo Lacerda <gustavogou22@gmail.com>
2025-05-24 07:27:14 -0300

    chore: add DefaultLoginComponent

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    DefaultLoginLayoutComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
